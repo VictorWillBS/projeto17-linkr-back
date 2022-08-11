de7465314ad27b637a23dfe56851de9f20f81cab
Paulo <pc5783346@gmail.com>
2022-08-10 21:32:01 -0300

    feat: agora a rota '/posts/:offset' retorna as metatags da url

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,16 +1,34 @@
+import urlMetadata from "url-metadata";
 import postRepository from "../repositories/postRepository.js";
 
 export async function postPublication(req, res) {
-  const { session } = res.locals;
-  const { url, content } = req.body;
+  try {
+    const { session } = res.locals;
+    const { url, content } = req.body;
 
-  if (!content) {
-    await postRepository.postPublicationUrl(session.userId, url);
+    const {
+      title, image, description
+    } = await urlMetadata(url);
+    const {
+      rows: metadataList 
+    } = await postRepository.getMetadatas();
+
+    if(!metadataList.some(metadata => metadata.url === url)){
+      await postRepository.postMetadata(
+        url, title, description, image
+      );
+    };
+
+    if (!content) {
+      await postRepository.postPublicationUrl(session.userId, url);
+      return res.sendStatus(201);
+    }
+
+    await postRepository.postPublicationFull(session.userId, url, content);
     return res.sendStatus(201);
+  } catch(e) {
+    res.status(500).send(e);
   }
-
-  await postRepository.postPublicationFull(session.userId, url, content);
-  return res.sendStatus(201);
 }
 
 export async function showPosts(req, res) {
@@ -19,10 +37,10 @@ export async function showPosts(req, res) {
     const { session } = res.locals;
 
     const {
-      rows: postList
-    } = await postRepository.getPosts(session.userId, offset);
+      rows: userPostList
+    } = await postRepository.getPostsbyUserId(session.userId, offset);
 
-    res.status(200).send(postList);
+    res.status(200).send(userPostList);
   } catch(e) {
     res.status(500).send(e);
   }
--- src/repositories/postRepository.js
+++ src/repositories/postRepository.js
@@ -20,11 +20,32 @@ async function postPublicationFull(userId, url, content) {
   );
 }
 
-async function getPosts(userId, offSet) {
+async function postMetadata(url, title, description, image){
   return connection.query(`
-    SELECT * FROM posts
+    INSERT INTO metadatas (
+      url, title, description, image
+    ) VALUES (
+      $1, $2, $3, $4
+    )
+  `, [
+    url, title, description, image
+  ]);
+}
+
+async function getPostsbyUserId(userId, offSet) {
+  return connection.query(`
+    SELECT
+      posts."userId" AS "userId",
+      posts.content AS "postContent",
+      posts.url AS url,
+      metadatas.title AS "urlTitle",
+      metadatas.description AS "urlDescription",
+      metadatas.image AS "urlImage"
+    FROM posts
+    JOIN metadatas ON
+    posts.url = metadatas.url
     WHERE "userId" = $1
-    ORDER BY "createdAt"
+    ORDER BY posts."createdAt"
     DESC LIMIT 20
     OFFSET $2
   `, [
@@ -32,10 +53,18 @@ async function getPosts(userId, offSet) {
   ]);
 }
 
+async function getMetadatas() {
+  return connection.query(`
+    SELECT * FROM metadatas
+  `, [url]);
+}
+
 const postRepository = {
   postPublicationUrl,
   postPublicationFull,
-  getPosts
+  postMetadata,
+  getPostsbyUserId,
+  getMetadatas
 };
 
 export default postRepository;
